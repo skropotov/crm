45630fe24f2f092b8cd4f39c8cffba0f826450fa
Sergey Kropotov <skropotov@yandex.ru>
2021-04-25 16:23:28 +0300

    change error handling for username integrity constraint error

--- api/src/main/java/com/skropotov/crm/exceptions/MainExceptionHandler.java
+++ api/src/main/java/com/skropotov/crm/exceptions/MainExceptionHandler.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityNotFoundException;
 
 import org.hibernate.StaleObjectStateException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,12 +30,8 @@ public class MainExceptionHandler {
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	@ResponseBody
 	public ErrorDTO handleSQLIntegrityConstraint(SQLIntegrityConstraintViolationException e) {
-		String errorMessage = e.getMessage();
 		if (e.getMessage().contains(User.usernameConstraintName)) {
-			int startIndex = errorMessage.indexOf("'") + 1;
-			int endIndex = errorMessage.indexOf("'", startIndex);
-			String username = errorMessage.substring(startIndex, endIndex);
-			return new ErrorDTO("USER", "Username '" + username + "' already exists");
+			return new ErrorDTO("USER", "Username already exists");
 		}
 		return new ErrorDTO("OBJECT", e.getMessage());
 	}
