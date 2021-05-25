9b966d03b4bd4b60d02f647083f2459b7e5724ac
Sergey Kropotov <skropotov@yandex.ru>
2021-05-25 23:17:51 +0300

    fix CORS

--- api/src/main/java/com/skropotov/crm/controllers/ProductController.java
+++ api/src/main/java/com/skropotov/crm/controllers/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.skropotov.crm.services.ProductService;
 import com.skropotov.crm.transfers.ProductDto;
 
 @RestController
+@CrossOrigin
 public class ProductController {
 	@Autowired
 	ProductService productService;
